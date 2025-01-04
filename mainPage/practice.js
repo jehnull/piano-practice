851749e14982b2c4fa89d435f54e7c5de559bea3
jehnull <119625905+jehnull@users.noreply.github.com>
2025-01-03 23:20:11 -0800

    Main page MIDI Check (#1)

---
+++ mainPage/practice.js
@@ -0,0 +1,28 @@
+//check if browser supports MIDI first
+if (navigator.requestMIDIAccess) {
+    navigator.requestMIDIAccess().then(MIDIsuccess, MIDIfail);
+}
+
+//if requestMIDIAccess returns success
+function MIDIsuccess(midiAccess) {
+    console.log(midiAccess);
+    console.log("MIDI Connected!");
+
+    const inputs = midiAccess.inputs;
+    console.log("inputs: ", inputs);
+
+    inputs.forEach((input) => {
+        document.getElementById("midiDeviceName").innerHTML = input.name;
+        console.log(input.name);
+    });
+}
+
+console.log(Tonal.Chord.detect(["D", "F#", "A", "C"])); // => ["D7"]
+console.log(Tonal.Chord.detect(["F#", "A", "C", "D"])); // => ["D7/F#"]
+
+//if requestMIDIAccess returns any failures
+function MIDIfail() {
+    console.log('Could not connect MIDI');
+    const midiAlert = document.getElementById('midiFailAlert');
+    midiAlert.classList.remove('d-none');
+}
