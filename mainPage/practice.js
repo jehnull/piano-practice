63c7045f3d4fa73a3fca5c3c27f8be196edece0e
juhnull <jehnull28@gmail.com>
2025-01-03 23:05:42 -0800

    Update practice.js

--- mainPage/practice.js
+++ mainPage/practice.js
@@ -1,25 +1,25 @@
 //check if browser supports MIDI first
 if (navigator.requestMIDIAccess) {
-  navigator.requestMIDIAccess().then(MIDIsuccess, MIDIfail);
+    navigator.requestMIDIAccess().then(MIDIsuccess, MIDIfail);
 }
 else {
-    const noMIDIBrowser = document.getElementById('noMIDIBrowser');
+    const noMIDIBrowsers = document.getElementById('noMIDIBrowser');
     document.getElementById("noBrowser").innerHTML = "AYYY";
-    noMIDIBrowser.classList.remove('d-none');
+    noMIDIBrowsers.classList.remove('d-none');
 }
 
 //if requestMIDIAccess returns success
 function MIDIsuccess(midiAccess) {
-  console.log(midiAccess);
-  console.log("MIDI Connected!");
+    console.log(midiAccess);
+    console.log("MIDI Connected!");
 
-  const inputs = midiAccess.inputs;
-  console.log("inputs: ", inputs);
+    const inputs = midiAccess.inputs;
+    console.log("inputs: ", inputs);
 
-  inputs.forEach((input) => {
-    document.getElementById("midiDeviceName").innerHTML = input.name;
-    console.log(input.name);
-  });
+    inputs.forEach((input) => {
+        document.getElementById("midiDeviceName").innerHTML = input.name;
+        console.log(input.name);
+    });
 }
 
 console.log(Tonal.Chord.detect(["D", "F#", "A", "C"])); // => ["D7"]
@@ -27,7 +27,7 @@ console.log(Tonal.Chord.detect(["F#", "A", "C", "D"])); // => ["D7/F#"]
 
 //if requestMIDIAccess returns any failures
 function MIDIfail() {
-  console.log('Could not connect MIDI');
-  const midiAlert = document.getElementById('midiFailAlert');
-  midiAlert.classList.remove('d-none');
+    console.log('Could not connect MIDI');
+    const midiAlert = document.getElementById('midiFailAlert');
+    midiAlert.classList.remove('d-none');
 }
